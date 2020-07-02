6c7af002a966a8e8cfa8a4e54cd532452ceecaa5
Artur Gonzallez <arturgsb@gmail.com>
2020-07-02 07:34:05 -0300

    Adicionado integração com plugin-update-checker

--- wp-plugin-example.php
+++ wp-plugin-example.php
@@ -18,3 +18,32 @@ function wp_plugin_example_add_message_footer($content) {
 
 add_filter('the_content', 'wp_plugin_example_add_message_footer');
 
+
+require_once 'plugin-update-checker/plugin-update-checker.php';
+
+
+/*
+ * Plugin Update Checker Setting
+ *
+ * @see https://github.com/YahnisElsts/plugin-update-checker for more details.
+ */
+function my_plugin_update_checker_setting() {
+	if ( ! is_admin() || ! class_exists( 'Puc_v4_Factory' ) ) {
+		return;
+	}
+
+	$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+		'https://github.com/hargon/wp_plugin_example/',
+		__FILE__,
+		'wp_plugin_example'
+	);
+	
+	// (Opcional) If you're using a private repository, specify the access token like this:
+	//$myUpdateChecker->setAuthentication('your-token-here');
+
+	// (Opcional) Set the branch that contains the stable release.
+	//$myUpdateChecker->setBranch('stable-branch-name');
+}
+
+add_action( 'admin_init', 'my_plugin_update_checker_setting' );
+
